ac5c10155aa3d0536ac4c7434c7ea700dd3df6a5
Czinke Márton <czinke.marci@gmail.com>
2020-11-12 23:10:39 +0100

    Implement select tile and deselect tile

--- Torpedo/MainWindow.xaml.cs
+++ Torpedo/MainWindow.xaml.cs
@@ -20,16 +20,17 @@ namespace Torpedo
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int GameWidth = 5;
-        private int GameHeight = 5;
-        bool[,] tableLayout = new bool[5, 5];
+        const int GameWidth = 5;
+        const int GameHeight = 5;
+        int[,] tableLayout = new int[GameWidth, GameHeight];
+        private int shipCount = 3;
         
 
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void DrawPoint(Vector position, Brush brush)
+        private void DrawPoint(Vector position, Brush brush,string Id)
         {
             var shape = new Rectangle();
             shape.Fill = brush;
@@ -37,6 +38,9 @@ namespace Torpedo
             var unitY = gameCanvas.Height / GameHeight;
             shape.Width = unitX;
             shape.Height = unitY;
+            shape.Stroke = Brushes.Black;
+            shape.StrokeThickness = 1.5;
+            shape.Uid = Id;
             Canvas.SetLeft(shape, position.X * unitX);
             Canvas.SetTop(shape, position.Y * unitY);
             gameCanvas.Children.Add(shape);
@@ -47,35 +51,38 @@ namespace Torpedo
             var mousePosition = Mouse.GetPosition(gameCanvas);
             var mousePositionX = mousePosition.X;
             var mousePositionY = mousePosition.Y;
-            clickedPoint.Text = mousePosition.X.ToString() + ":" + mousePosition.Y.ToString();
             var lowerLimitX = 0;
             var lowerLimitY = 0;
-            var upperLimitX = 1;
-            var upperLimitY = 1;
             for (int i = 1; i < 5; i++)
             {
                 if (i*(gameCanvas.Width/GameWidth) < mousePositionX)
                 {
                     lowerLimitX = i;
-                    upperLimitX = i + 1;
                 }
                 if(i*(gameCanvas.Height/GameHeight) < mousePositionY)
                 {
                     lowerLimitY = i;
-                    upperLimitY = i + 1;
                 }
             }
             var tileVector = new Vector(lowerLimitX, lowerLimitY);
-            if (tableLayout[lowerLimitX, lowerLimitY] != true)
+            var generatedId = lowerLimitX + "" + lowerLimitY;
+            textTest2.Text = "Cordinates: " + lowerLimitX + ":" + lowerLimitY + ", value:" + tableLayout[lowerLimitX, lowerLimitY].ToString();
+            if (tableLayout[lowerLimitX, lowerLimitY] != 1 && shipCount > 0)
             {
-                tableLayout[lowerLimitX, lowerLimitY] = true;
-                DrawPoint(tileVector, Brushes.AliceBlue);
+                tableLayout[lowerLimitX, lowerLimitY] = 1;
+                DrawPoint(tileVector, Brushes.Red,generatedId);
+                shipCount -= 1;
             }
-            else
+            else if(tableLayout[lowerLimitX, lowerLimitY] != 0 && shipCount >= 0)
             {
-                tableLayout[lowerLimitX, lowerLimitY] = false;
-                DrawPoint(tileVector, Brushes.White);
+                tableLayout[lowerLimitX, lowerLimitY] = 0;
+                gameCanvas.Children.Remove(FindUid(gameCanvas, generatedId));
+                shipCount +=1;
             }
+            
+            textTest1.Text = shipCount.ToString();
+            textTest2.Text = generatedId;
+            
             string cordinatesAsString = "";
             for (int i = 0; i < 5; i++)
             {
@@ -84,8 +91,29 @@ namespace Torpedo
                     cordinatesAsString += ","+tableLayout[i, j].ToString();
                 }
             }
-            textCordinates.Text = cordinatesAsString;
             
+        }
+        private static UIElement FindUid(DependencyObject parent, string uid)
+        {
+            var count = VisualTreeHelper.GetChildrenCount(parent);
+            if (count == 0) return null;
+            for (int i = 0; i < count; i++)
+            {
+                var el = VisualTreeHelper.GetChild(parent, i) as UIElement;
+                if (el == null) continue;
+                if (el.Uid == uid)
+                {
+                    return el;
+                }
+            }
+            return null;
+        }
+
+
+        private void ConfirmChoosing(object sender, RoutedEventArgs e)
+        {
+            //Pass the matrix
+            //OpenOtherWindows
         }
     }
 }
